2413075f73e1dfa305d54a15b4b69480e2e7ad47
Roman <rmn-08@yandex.ru>
2023-12-07 14:00:22 +0500

    initial

---
+++ salesManager/Controllers/LogInViewContoller.swift
@@ -0,0 +1,212 @@
+//
+//  File.swift
+//  salesManager
+//
+//  Created by Роман Кокорев on 05.12.2023.
+//
+
+import UIKit
+
+final class LogInViewContoller: UIViewController{
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.clipsToBounds = true
+        imageView.alpha = 0.3
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .systemGray6
+        textField.textColor = .systemGray
+        textField.placeholder = "Email или телефон"
+        textField.layer.borderWidth = 0.5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 10, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 10
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .systemGray6
+        textField.isSecureTextEntry = true
+        textField.textColor = .systemGray
+        textField.placeholder = "Пароль"
+        textField.layer.borderWidth = 0.5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 10, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 10
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Войти", for: .normal)
+        button.backgroundColor = .systemGray
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(self.signUpButtonButtonClicked), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var registrationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Регистрация", for: .normal)
+        button.backgroundColor = .systemGray
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(self.registrationButtonClicked), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var invalidLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.textColor = .lightGray
+        label.font = .systemFont(ofSize: 12)
+        label.numberOfLines = 8
+        label.contentMode = .scaleToFill
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
+    }()
+    
+    private lazy var validationData = ValidationData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        setupView()
+        setupContraint()
+    }
+
+    private func setupNavigationBar() {
+        navigationItem.backButtonTitle = "Назад"
+        navigationController?.navigationBar.tintColor = .systemGray
+    }
+        
+    func setupView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(logoImageView)
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signUpButton)
+        view.addSubview(registrationButton)
+        view.addSubview(invalidLabel)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapKeyboardOff(_:)))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func setupContraint() {
+        let screenWidth = UIScreen.main.bounds.width
+        
+        NSLayoutConstraint.activate([
+            loginTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginTextField.heightAnchor.constraint(equalToConstant: 50),
+            loginTextField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -5),
+            
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50),
+            passwordTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 5),
+                        
+            logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logoImageView.bottomAnchor.constraint(equalTo: loginTextField.topAnchor, constant: -25),
+            logoImageView.widthAnchor.constraint(equalToConstant: screenWidth - 100),
+            
+            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            signUpButton.heightAnchor.constraint(equalToConstant: 50),
+            signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            
+            registrationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            registrationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            registrationButton.heightAnchor.constraint(equalToConstant: 50),
+            registrationButton.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 10),
+            
+            invalidLabel.topAnchor.constraint(equalTo: registrationButton.bottomAnchor),
+            invalidLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            invalidLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    private func validEmail(email: String) -> Bool {
+        let emailReg = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let validEmail = NSPredicate(format:"SELF MATCHES %@", emailReg)
+        return validEmail.evaluate(with: email)
+    }
+
+    private func validPassword(password : String) -> Bool {
+        let passwordReg =  ("(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[@#$%^&*]).{8,}")
+        let passwordTesting = NSPredicate(format: "SELF MATCHES %@", passwordReg)
+        return passwordTesting.evaluate(with: password) && password.count > 6
+    }
+    
+    @objc func tapKeyboardOff(_ sender: Any) {
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
+    @objc func signUpButtonButtonClicked() {
+        guard let email = loginTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        let enteredEmail = validEmail(email: email)
+        let enteredPassword = validPassword(password: password)
+        
+        if email.isEmpty && password.isEmpty {
+            loginTextField.shake()
+            passwordTextField.shake()
+        } else if email.isEmpty {
+            loginTextField.shake()
+        } else if password.isEmpty {
+            passwordTextField.shake()
+        } else {
+            if !enteredPassword && !enteredEmail {
+                invalidLabel.text = validationData.invalidEmailAndPassword
+                invalidLabel.isHidden = false
+                passwordTextField.shake()
+                loginTextField.shake()
+            } else if !enteredPassword {
+                invalidLabel.text = validationData.invalidPassword
+                invalidLabel.isHidden = false
+                passwordTextField.shake()
+            } else if !enteredEmail {
+                invalidLabel.text = validationData.invalidEmail
+                invalidLabel.isHidden = false
+                loginTextField.shake()
+            } else {
+                if (enteredEmail && enteredPassword) && (loginTextField.text != validationData.defaultLogin || passwordTextField.text != validationData.defaultPassword) {
+                    let alert = UIAlertController(title: "Неверный логин или пароль", message: nil, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(alert, animated: true, completion: nil)
+                } else {
+                    navigationController?.pushViewController(ViewController(), animated: true)
+                    invalidLabel.isHidden = true
+                }
+            }
+        }
+        
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
+    @objc func registrationButtonClicked() {
+        navigationController?.pushViewController(RegistrationViewController(), animated: true)
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
+}
+
+
---
+++ salesManager/Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  salesManager
+//
+//  Created by Роман Кокорев on 05.12.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
