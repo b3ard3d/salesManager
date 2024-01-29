b54cb9cd17406b18fe34e3c05e59b75905e186bb
Roman <rmn-08@yandex.ru>
2024-01-29 09:25:10 +0500

    До оптимизации networkManager

---
+++ salesManager/Models/ContactColdClients.swift
@@ -0,0 +1,8 @@
+//
+//  ContactColdClients.swift
+//  salesManager
+//
+//  Created by Роман Кокорев on 26.01.2024.
+//
+
+import Foundation
---
+++ salesManager/Models/ContactPartners.swift
@@ -0,0 +1,8 @@
+//
+//  ContactPartners.swift
+//  salesManager
+//
+//  Created by Роман Кокорев on 26.01.2024.
+//
+
+import Foundation
